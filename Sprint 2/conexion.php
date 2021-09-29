681063e7359894752680622cb545b9c797789f5b
jayler0203 <90487437+jayler0203@users.noreply.github.com>
2021-09-29 13:21:08 -0500

    Delete conexion.php

--- Sprint 2/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection=mysqli_connect("localhost", "root", "", "registro");
-
-if($connection){
-    echo "Sucesfully connected to the database :)";
-}else{
-    echo "something went wrong, cannot connect to the data base ⚠";
-}
-?>
